ff01fd7af042a089868f12eb06afbb8d139a8971
aryan-negi-93 <134124253+aryan-negi-93@users.noreply.github.com>
2024-04-13 03:20:29 +0530

    add database setup

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import { config } from './src/config/config';
 import app from './src/app'
+import connectDb from './src/config/db';
 
 
 
-const startServer = () => {
+const startServer = async () => {
+    // connect database
+    await connectDb()
+
     const port = config.port || 3000;
 
     app.listen(port, () => {
---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+
+import { config } from "./config"
+import mongoose from "mongoose"
+
+const connectDb = async () => {
+    try {
+
+        mongoose.connection.on('connected', () => {
+            console.log("connected to database successfully ");
+        })
+
+        mongoose.connection.on('error', (err) => {
+            console.log('error in connecting to database!', err);
+        })
+
+        await mongoose.connect(config.databaseUrl as string)
+
+    } catch (err) {
+        console.error("failed to connect to database ", err)
+        process.exit(1); // ager error aya toh server chalakar kya fayda wo stop ho jayga esa
+    }
+}
+export default connectDb
